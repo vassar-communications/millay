a9634766d5bf82f51c6ff8ee35edd6b2d462b7af
Jeff Macaluso <samacaluso@vassar.edu>
2017-01-24 16:05:51 -0500

    Initial commit
    Standalone subdirectory in Special Collections Exhibition Highlights

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return 
+array(
+    'siteDesignType' => 'academic',
+    'vassarCollegeAppend' => '',
+    'subSiteNavigationAdditional' => array('grandparent','All Exhibits'),
+    'typekitID' => 'gon4vhs',
+    'siteNameHeader' => "<b class='u-Masthead__siteName--line1'>Edna St.Vincent Millay: Treasures from Steepletop</b>",
+    'siteNameSubhead' => "Catherine Pelton Durrell '25 <br />Archives &amp; Special Collections Library",
+);
